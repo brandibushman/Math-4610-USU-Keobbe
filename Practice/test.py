b050057ee13866f47ad6a1cebf1df70344fe5c71
brandibushman <42750004+brandibushman@users.noreply.github.com>
2018-09-15 17:50:28 -0600

    Update test.py

--- Practice/test.py
+++ Practice/test.py
@@ -6,3 +6,19 @@ def prob1(u,v):
     B=(abs(u-v))/(abs(u))
     # print(B)
     return B
+
+
+
+
+def demo_routine(num):
+ print('I am a demo function')
+ if num % 2 == 0:
+  return(True)
+ else:
+  return(False)
+
+num = int(input('Enter a number:'))
+if demo_routine(num) is True:
+ print(num, 'is an even number')
+else:
+ print(num, 'is an odd number')
